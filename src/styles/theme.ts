c66a5d38364abe3b3fa30c03e45ef1e3e598866a
kimtaehyun <tjdrkr2580@naver.com>
2023-05-12 14:08:28 +0900

    add : theme type 및 theme color

---
+++ src/styles/theme.ts
@@ -0,0 +1,13 @@
+export type themeTypes = {
+  coffeeColor: string;
+  backgroundColor: string;
+  primaryColor: string;
+  textColor: string;
+};
+
+export const theme: themeTypes = Object.freeze({
+  coffeeColor: "#3C2A21",
+  backgroundColor: "#1A120B",
+  primaryColor: "#D5CEA3",
+  textColor: "#FBF8F1",
+});
