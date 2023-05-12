7a8d7d44fd770d3247db838887b40bf0d022bf80
kimtaehyun <tjdrkr2580@naver.com>
2023-05-13 03:44:16 +0900

    add : recoil state 모달

--- src/utils/atoms.ts
+++ src/utils/atoms.ts
@@ -1,6 +1,12 @@
 import { atom } from "recoil";
+import { v4 } from "uuid";
 
 export const isDarkModeState = atom<boolean>({
   key: "isDarkMode",
+  default: false,
+});
+
+export const modalState = atom<boolean>({
+  key: `state${v4()}`,
   default: false,
 });
