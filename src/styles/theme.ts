e1415afae6dfd725caaa965b0cde9864a984af60
kimtaehyun <tjdrkr2580@naver.com>
2023-05-12 14:09:35 +0900

    add : darktheme 추가

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -5,9 +5,16 @@ export type themeTypes = {
   textColor: string;
 };
 
-export const theme: themeTypes = Object.freeze({
+export const lightTheme: themeTypes = Object.freeze({
   coffeeColor: "#3C2A21",
   backgroundColor: "#1A120B",
   primaryColor: "#D5CEA3",
   textColor: "#FBF8F1",
+});
+
+export const darkTheme: themeTypes = Object.freeze({
+  coffeeColor: "#3C2A21",
+  textColor: "#1A120B",
+  primaryColor: "#D5CEA3",
+  backgroundColor: "#FBF8F1",
 });
